f43a0c6065a7327ab0873e608141b3e74a4cfb22
Laurent Ellerbach <laurent@ellerbach.net>
2015-11-24 14:52:21 +0100

    New files for Device Explorer part

---
+++ DeviceExplorer/public/javascripts/chkmsg.js
@@ -0,0 +1,17 @@
+﻿var xhr = new XMLHttpRequest();
+var iswaiting = false;
+
+function chkmsg() {
+    iswaiting = true;
+    xhr.open('GET', '/msgstatus');
+    xhr.send(null);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.responseText != '')
+                boxMSG.innerHTML = xhr.responseText;
+        }
+    }
+    iswaiting = false;
+}
+
+setInterval(function () { if (!iswaiting) chkmsg(); }, 1000);
